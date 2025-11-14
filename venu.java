99b15364d162b7662f6e2cc86cf5ae278d9417f5
Venu <venubade3421@gmail.com>
2025-11-14 10:41:44 +0530

    updated

--- venu.java
+++ venu.java
@@ -4,11 +4,13 @@ public class Venu
   {
     System.out.println("love you");
 
-
     System.out.pirntln("i am using stash for after backup");
+
 
     System.out.println("i using git stash");
 
 System.out.pirntln("i am using git revart");
+
+
   }
 }
