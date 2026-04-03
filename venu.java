6ffca374d61c45b9abfb7f7747058ead56d52756
Venu <147303386+Venubade123@users.noreply.github.com>
2026-04-04 00:56:02 +0530

    Update venu.java

--- venu.java
+++ venu.java
@@ -12,11 +12,10 @@ public class Venu
 
 System.out.pirntln("i am using git revart");
 system.out.println("i am new to this");
-<<<<<<< HEAD
+
 System.out.println("i miss you ra");
-=======
+
     System.out.println("i love you to ra");
->>>>>>> 4a3f89b5959888a95212802e9c2196b9a4a97d70
 
 
 
